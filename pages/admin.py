8a971d2ef288237abc12997207912b541d3272ec
Revy1664 <steinsgate95@mail.ru>
2022-10-04 20:52:04 +0600

    add theme model, add ckeditor for description field

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,16 @@
+from django import forms
 from django.contrib import admin
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
-# Register your models here.
+from .models import Theme
+
+class ThemeAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Theme
+        fields = "__all__"
+
+class ThemeAdmin(admin.ModelAdmin):
+    form = ThemeAdminForm
+
+admin.site.register(Theme, ThemeAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+
+from .models import Theme
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+	model = Theme
 	template_name = "pages/index.html"
+	context_object_name = "themes"
